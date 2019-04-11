ccadd78e080777caa12418e331d877bc57d39062
charliethomasdavis <charles.tb.davis@gmail.com>
2019-04-10 19:47:04 -0500

    Add clickable grid that changes colors

--- src/game.py
+++ src/game.py
@@ -5,15 +5,33 @@ Created by: Fernando Rodriguez, Charles Davis, Paul Rogers
 
 """
 
+#TODO: Create Map class that will draw the grid and hold tiles
+#TODO: Create Unit class for unit actions and data
+#TODO: Move code from here into relevant classes
+
 import sys
 import pygame
 
+from gamestate import GameState
+
 # Window Size
-WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 255
+WINDOW_HEIGHT = 255
+
+#DEBUG: grid size
+GRID_WIDTH = 23
+GRID_HEIGHT = 23
+GRID_MARGIN = 2
+
+# Constants
+TILE_ROWS = 10
+TILE_COLS = 10
 
 # Colors
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
 class Game:
 
@@ -30,6 +48,10 @@ class Game:
         self.screen = pygame.display.set_mode(self.screen_res)
 
         #self.entities = pygame.sprite.Group()
+        self.rows = TILE_ROWS
+        self.cols = TILE_COLS
+        self.game_state = GameState()
+        self.grid = self.game_state.tiles
 
         self.clock.tick(30)
 
@@ -50,6 +72,23 @@ class Game:
                 # add code for terminating network connection
                 pygame.quit()
                 sys.exit(0)
+            # User clicks mouse
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if self.mousepos[0] not in range(GRID_WIDTH * (self.cols + 1)):
+                    break
+                if self.mousepos[1] not in range(GRID_HEIGHT * (self.rows + 1)):
+                    break
+                #DEBUG: print column and row of click
+                column = self.mousepos[0] // (GRID_WIDTH + GRID_MARGIN)
+                row = self.mousepos[1] // (GRID_HEIGHT + GRID_MARGIN)
+                if (self.grid[row][column] == 0):
+                    self.grid[row][column] = 1
+                elif (self.grid[row][column] == 1):
+                    self.grid[row][column] = 2
+                else:
+                    self.grid[row][column] = 0
+                print("Click ", self.mousepos, "Grid coords: ", row, column)
+                
     
     def tick(self):
         self.ttime = self.clock.tick()
@@ -57,7 +96,24 @@ class Game:
         self.keys_pressed = pygame.key.get_pressed()
 
     def draw(self):
-        self.screen.fill(BLACK)
+        self.screen.fill(WHITE)
+
+        for row in range(self.rows):
+            for col in range(self.cols):
+                color = BLACK
+                if self.grid[row][col] == 1:
+                    color = GREEN
+                elif self.grid[row][col] == 2:
+                    color = RED
+                pygame.draw.rect(self.screen,
+                                 color,
+                                 [(GRID_MARGIN + GRID_WIDTH) * col + GRID_MARGIN,
+                                  (GRID_MARGIN + GRID_HEIGHT) * row + GRID_MARGIN,
+                                  GRID_WIDTH,
+                                  GRID_HEIGHT])
+
+        pygame.display.update()
+        #DEBUG: display grid here
 
         #update map layers?
 
--- src/gamestate.py
+++ src/gamestate.py
@@ -1,20 +1,20 @@
 import enum
 
+# Constants
+TILE_ROWS = 10
+TILE_COLS = 10
+    
 class GameState:
-""" Represents the game board and state of
-    each tile. The GameState.data attribute
-    contains the entire dictionary to be
-    sent across the network.
-"""
-    # Constants
-    TILE_ROWS = 5
-    TILE_COLS = 9
+# Represents the game board and state of
+# each tile. The GameState.data attribute
+# contains the entire dictionary to be
+# sent across the network.
 
     def __init__(self):
 
         # Initialize 2D array holding Tile locations.
         # Fill with blank tiles.
-        self.tiles = [[Tiletype.blank for j in range(TILE_COLS)] for i in range(TILE_ROWS)]
+        self.tiles = [[0 for j in range(TILE_COLS)] for i in range(TILE_ROWS)]
 
         # Array holding locations of player1's
         # three characters.
